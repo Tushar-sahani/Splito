3f453a730b59ed01ff1d7d9966c1edd5118a4c3e
Tushar Sahani <tusharsahani000@gmail.com>
2022-11-13 16:13:42 +0530

    contribute
    =

--- contributequal/index.js
+++ contributequal/index.js
@@ -1,3 +1,7 @@
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>30);
+})
+
 // const searchButton = document.getElementById('search-button');
 // const searchInput = document.getElementById('search-input');
 // searchButton.addEventListener('click', () => {
@@ -11,14 +15,3 @@ let a = 6;
 let random = Math.floor(Math.random() * (10**a));
 key.innerHTML = random;
 
-const stickytop = document.getElementsByClassName('navbar');
-stickytop.addEventListener("scroll",()=>{
-    stickytop.classList.add("stickeytop");
-})
-// function myFunction(){
-//     const navbar = document.getElementsByClassName("navbar");
-//     navbar.addEventListener("scroll", myScript);
-// }
-// function myScript(){
-//     navbar.classList.add("stickytop");
-// }
